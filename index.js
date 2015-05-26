e397644c4c5c0c46e7715babfa10670298577ec8
Ansel Rosenberg <ansel01@gmail.com>
2015-05-26 14:39:32 -0700

    Add jwt middleware for /api/routes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var googleAuth = require('google-auth-library');
 var calendar = google.calendar('v3');
 var redis = require('redis');
 var client = redis.createClient();
+var jwt = require('jsonwebtoken');
 
 client.on('connect', function() {
   console.log('connected');
@@ -14,10 +15,11 @@ client.on('connect', function() {
 
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
+app.set('superSecret', 'anything');
 
 var main = require('./routes/main.js')(app);
 var authenticate = require('./routes/authentication')(app, apiRouter);
-var api = require('./routes/api.js')(app, apiRouter);
+var api = require('./routes/api.js')(app, apiRouter, jwt);
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
