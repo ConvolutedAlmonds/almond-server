b301f91fe76420f08996f4d8c54f4a5b31f8e2cf
Ansel Rosenberg <ansel01@gmail.com>
2015-06-05 19:22:03 -0700

    Move seconds to readable time converter to own file

---
+++ utils/time.js
@@ -0,0 +1,17 @@
+var moment = require('moment');
+
+/**
+ * Converts number of seconds to readable format - 1 hour 23 mins, etc. 
+ */
+module.exports = function(numSeconds) {
+  var result = '';
+  var duration = moment.duration(numSeconds, 'seconds');
+  var hours = Math.floor(duration.asHours());
+  var mins = Math.floor(duration.asMinutes()) - hours * 60;
+
+  if (hours) result += hours + 'h '
+
+  result += mins + 'm'
+
+  return result;
+};
