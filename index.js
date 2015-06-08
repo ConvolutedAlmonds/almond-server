588f01b116cee8fc17243804c230a7c1c0e213ed
Ansel Rosenberg <ansel01@gmail.com>
2015-06-07 21:20:10 -0700

    Attempt cors fix

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', "*");
-  // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-Access-Token, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-Access-Token, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
   next();
 });
 
