fe742c7342a964f1f4040ad87268939658ec1c54
Ansel Rosenberg <ansel01@gmail.com>
2015-06-09 20:04:25 -0700

    Add function for refreshing access tokens

--- utils/refresh.js
+++ utils/refresh.js
@@ -1,23 +1,23 @@
-// var qs = require('querystring');
-// var Promise = require('bluebird');
-// var request = Promise.promisify(require('request'));
+var qs = require('querystring');
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
 
-// module.exports = function(refreshToken, credentials, callback) {
-//   var refreshEndpoint = 'https://www.googleapis.com/oauth2/v3/token';
+module.exports = function(refreshToken, credentials, callback) {
+  var refreshEndpoint = 'https://www.googleapis.com/oauth2/v3/token';
 
-//   console.log(credentials.installed.client_secret, credentials.installed.client_id, refreshToken)
+  console.log(credentials.installed.client_secret, credentials.installed.client_id, refreshToken)
 
-//   var refreshUrl = refreshEndpoint + '?' +
-//     'refresh_token=' + refreshToken +
-//     '&client_secret=' + credentials.installed.client_secret +
-//     '&client_id=' + credentials.installed.client_id +
-//     '&grant_type=refresh_token';
+  var refreshUrl = refreshEndpoint + '?' +
+    'refresh_token=' + refreshToken +
+    '&client_secret=' + credentials.installed.client_secret +
+    '&client_id=' + credentials.installed.client_id +
+    '&grant_type=refresh_token';
 
-//   request.post(refreshUrl, function (err, httpResponse, body) {
-//     callback(body)
-//   })
+  request.post(refreshUrl, function (err, httpResponse, body) {
+    callback(body)
+  })
 
 
-// };
+};
 
 
