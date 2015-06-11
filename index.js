1bbfabf13c6854f68b51eb2a5f8909ac267059d6
Ansel Rosenberg <ansel01@gmail.com>
2015-06-11 01:03:51 -0700

    Add error handling for illegal user destinations

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ var User = require('./db/models/user.js');
 var request = require('request');
 var moment = require('moment');
 var getNewAccessToken = require('./utils/refresh.js');
+var _ = require('underscore');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -43,31 +44,59 @@ app.use(function(req, res, next) {
  */
 app.use(function(req, res, next) {
 
+
   if (req.body.destAddress) {
 
     async.parallel({
       geocode: function(cb) {
         geocoder.geocode(req.body.destAddress, function(err, data) {
-          var coordinates = data.results[0].geometry;
-          req.body.destination = {};
-          req.body.destination.longitude = coordinates.location.lng.toString();
-          req.body.destination.latitude = coordinates.location.lat.toString();
-          cb(null, true)
+
+          if (err || !data.results[0] || isProbablyWrong) {
+            // console.log('error geocoding:', err)
+            cb(true, null);
+          } else {
+
+            var isProbablyWrong = _.contains(data.results[0].types, 'subpremise');
+
+            if (isProbablyWrong) {
+              cb(true, null);
+            } else {
+              // console.log('Geocode results:', data.results[0])
+              var coordinates = data.results[0].geometry;
+              req.body.destination = {};
+              req.body.destination.longitude = coordinates.location.lng.toString();
+              req.body.destination.latitude = coordinates.location.lat.toString();
+              cb(null, true);
+            }
+          }
         });
       },
       reverseGeocode: function(cb) {
         var latitude = req.body.origin.latitude;
         var longitude = req.body.origin.longitude;
         geocoder.reverseGeocode(Number(latitude), Number(longitude), function(err, data) {
-          if (err) console.log('error reverse geocoding', err);
-          req.body.origin.address = data.results[0].formatted_address;
-          cb(null, true);
+          if (err) {
+            // console.log('error reverse geocoding', err);
+            cb(err, null);
+          } else {
+            // console.log('reverseGeocode results:', data)
+            req.body.origin.address = data.results[0].formatted_address;
+            cb(null, true);
+          }
         })
 
       },
     },
       function(err, results) {
-        next()
+        // console.log('geocode final error:', err);
+        // console.log('geocode final results:', results);
+        if (err) {
+          res.status(400);
+          res.end()
+        } else {
+          next()
+
+        }
     });
 
   } else {
